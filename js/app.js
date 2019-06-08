54ad04538aba9c9b69b55a5b47bfc680645f05db
horlumiday <oluwadareysamuel@yahoo.com>
2019-06-08 17:25:27 +0100

    change event listener added to all  input[type=range] & sliderEffect function added

--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 const eventListener = () => {
 	const header = document.querySelector('#header');
 	const file = document.querySelector('input[type=file]');
+	const effect = document.querySelector('.effect');
 
 	//event listeners
 	header.addEventListener('click', toggleFile);
 	file.addEventListener('change', loadImage);
-	
+	effect.addEventListener('change', sliderEffect);
 }
 
 const toggleFile = (event) => {
@@ -41,6 +42,26 @@ const loadImage = () => {
 	//hide selected element
 	appName.style.display = "none";
 
+}
+
+const sliderEffect = (event) => {
+	let target = event.target;
+	let img = document.querySelector('.img');
+	
+	if (target.tagName === 'INPUT') {
+		const inputRange = document.querySelectorAll('input[type=range]');
+
+		//loop through all inputs and listen for a click event
+		inputRange.forEach((input) => {
+			input.addEventListener('click', (event) => {
+				
+				let dataset = target.dataset.name;
+				document.documentElement.style.setProperty(`--${target.id}`, `${target.value+dataset}`);
+
+			});
+		})
+
+	}
 }
 
 //call function
