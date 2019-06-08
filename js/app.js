dfa5b21ccbfb37af1316c1b2a1350928e5670940
horlumiday <oluwadareysamuel@yahoo.com>
2019-06-08 21:46:26 +0100

    fixed invert(input[type=range] data-default value , added reset function(reset all sliders to default

--- js/app.js
+++ js/app.js
@@ -2,13 +2,14 @@ const eventListener = () => {
 	const header = document.querySelector('#header');
 	const file = document.querySelector('input[type=file]');
 	const effect = document.querySelector('.effect');
+	const resetButton = document.querySelector('#reset');
 
 	//event listeners
 	header.addEventListener('click', toggleFile);
 	file.addEventListener('change', loadImage);
 	effect.addEventListener('change', sliderEffect);
 	effect.addEventListener('change', blendModes);
-
+	resetButton.addEventListener('click', reset);
 }
 
 const toggleFile = (event) => {
@@ -49,19 +50,23 @@ const loadImage = () => {
 const sliderEffect = (event) => {
 	let target = event.target;
 	let img = document.querySelector('img');
-	
+
+	//remove class from img element
+	img.classList.remove('img');
+
 	if (target.tagName === 'INPUT') {
+
 		const inputRange = document.querySelectorAll('input[type=range]');
 
 		//loop through all inputs and listen for a click event
 		inputRange.forEach((input) => {
-			input.addEventListener('click', (event) => {
-				
-				let dataset = target.dataset.name;
-				document.documentElement.style.setProperty(`--${target.id}`, `${target.value+dataset}`);
+				input.addEventListener('click', (event) => {
+					let dataset = input.dataset.name;
+					document.documentElement.style.setProperty(`--${input.id}`, `${input.value+dataset}`);
+				})
+			}
 
-			});
-		})
+		)
 
 	}
 }
@@ -75,6 +80,28 @@ const blendModes = (event) => {
 	}
 }
 
+const reset = (event) => {
+	let target = event.target;
 
+	const form = document.querySelector('form');
+	const inputRange = document.querySelectorAll('input[type=range]');
+	let img = document.querySelector('img');
+	
+	
+	//reset sliders and blend mode
+	form.reset();
+	
+	//add class to image element
+	img.classList.add('img');
+	
+	//loop through input[type=range] and set css variables to default value
+	inputRange.forEach((input) => {
+		let dataset = input.dataset;
+
+		document.documentElement.style.setProperty(`--${input.id}`, `${dataset.default+dataset.name}`);
+	
+	})
+
+}
 //call function
 eventListener();
